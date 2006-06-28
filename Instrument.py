2e2895d8c442f3758902093c2cd404f94fddc92c
laszlop <>
2006-06-28 01:32:10 +0000

    Fixed recording when there is a space in the file path. Thanks to Paul again for the regression testing. This was broken by the massive r419.

--- Instrument.py
+++ Instrument.py
@@ -218,7 +218,7 @@ class Instrument(Monitored, CommandManaged):
 		self.tmpe.name = "Recorded audio"
 		self.tmpe.file = file
 
-		self.output = "audioconvert ! vorbisenc ! oggmux ! filesink location=%s"%file
+		self.output = "audioconvert ! vorbisenc ! oggmux ! filesink location=%s"%file.replace(" ", "\ ")
 		print "Using pipeline: alsasrc device=%s%s%s"%(self.input, self.effects, self.output)
 
 		self.recordingbin = gst.parse_launch("bin.( alsasrc device=%s %s %s )"%(self.input, self.effects, self.output))
