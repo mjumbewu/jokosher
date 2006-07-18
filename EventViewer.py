ac44360d163109003049fb875fe19b67c68e394c
laszlop <>
2006-07-18 22:57:12 +0000

    Update for my last commit. This should make the selection dissappear on every scroll, not just some like before.

--- EventViewer.py
+++ EventViewer.py
@@ -623,11 +623,13 @@ class EventViewer(gtk.DrawingArea):
 			self.queue_resize()
 			self.last_num_levels = len(self.event.levels)
 			self.currentScale = self.project.viewScale
+		
+		elif type(obj) == Project.Project:
 			# remove any existing selection in this event
 			self.Selection = [0,0]
 			if self.drawer.parent == self.lane.fixed:
 				self.lane.fixed.remove(self.drawer)
-
+		
 		self.queue_draw()
 
 	#_____________________________________________________________________
