30a45a70bdce0265081b646dcc78ead3b722494a
laszlop <>
2006-08-24 05:13:40 +0000

    * Changed all occurences of plugin to extension.  * Add three new API functions.

--- Extension.py
+++ Extension.py
@@ -6,38 +6,38 @@ import gettext
 _ = gettext.gettext
 
 # Define some constants
-PLUGIN_DIR_USER = os.path.expanduser('~/.jokosher/extensions/')
-PLUGIN_DIRS = [PLUGIN_DIR_USER, '/usr/lib/jokosher/extensions/']
-# add your own plugin dirs with envar JOKOSHER_PLUGIN_DIRS, colon-separated
-OVERRIDE_PLUGIN_DIRS = os.environ.get('JOKOSHER_EXTENSION_DIRS','')
-if OVERRIDE_PLUGIN_DIRS:
-	PLUGIN_DIRS = OVERRIDE_PLUGIN_DIRS.split(':') + PLUGIN_DIRS
-PREFERRED_PLUGIN_DIR = PLUGIN_DIRS[0]
+EXTENSION_DIR_USER = os.path.expanduser('~/.jokosher/extensions/')
+EXTENSION_DIRS = [EXTENSION_DIR_USER, '/usr/lib/jokosher/extensions/']
+# add your own extension dirs with envar JOKOSHER_EXTENSION_DIRS, colon-separated
+OVERRIDE_EXTENSION_DIRS = os.environ.get('JOKOSHER_EXTENSION_DIRS','')
+if OVERRIDE_EXTENSION_DIRS:
+	EXTENSION_DIRS = OVERRIDE_EXTENSION_DIRS.split(':') + EXTENSION_DIRS
+PREFERRED_EXTENSION_DIR = EXTENSION_DIRS[0]
 
 RESP_INSTALL = 9999
 RESP_REPLACE = 9998
 
-# Work out whether I'm being imported by a plugin that's being run directly
-# or whether I'm being imported by a plugin run by Jokosher
+# Work out whether I'm being imported by a extension that's being run directly
+# or whether I'm being imported by a extension run by Jokosher
 import inspect
-plugin_that_imported_me = inspect.currentframe().f_back
+extension_that_imported_me = inspect.currentframe().f_back
 try:
-	thing_that_imported_plugin = plugin_that_imported_me.f_back 
+	thing_that_imported_extension = extension_that_imported_me.f_back 
 except:
-	thing_that_imported_plugin = None
+	thing_that_imported_extension = None
 	
-if thing_that_imported_plugin is None and \
-			os.path.split(plugin_that_imported_me.f_code.co_filename)[1] != 'JokosherApp.py':
-	# the plugin is being run directly; pop up the error 
+if thing_that_imported_extension is None and \
+			os.path.split(extension_that_imported_me.f_code.co_filename)[1] != 'JokosherApp.py':
+	# the extension is being run directly; pop up the error 
 	try:
 		import gtk
 	except:
 		# no Gtk either! Print a message and die
 		import sys
-		print "This is a Jokosher plugin; it is not meant to be run directly."
-		print "To install it, move it to the directory %s\nand run Jokosher." % (PLUGIN_DIR_LOCAL)
+		print "This is a Jokosher extension; it is not meant to be run directly."
+		print "To install it, move it to the directory %s\nand run Jokosher." % (EXTENSION_DIR_LOCAL)
 		sys.exit(1)
-	d = gtk.MessageDialog(message_format="This is a Jokosher plugin, which needs "+\
+	d = gtk.MessageDialog(message_format="This is a Jokosher extension, which needs "+\
 	                      "to be installed. Would you like to install it?",
 	                      type=gtk.MESSAGE_ERROR)
 	d.add_buttons(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,'Install',RESP_INSTALL)
@@ -45,13 +45,13 @@ if thing_that_imported_plugin is None and \
 	ret = d.run()
 	d.destroy()
 	if ret == RESP_INSTALL:
-		plugin_path_and_file = plugin_that_imported_me.f_globals['__file__']
-		plugin_file_name = os.path.split(plugin_path_and_file)[1]
-		new_plugin_path_and_file = os.path.join(PREFERRED_PLUGIN_DIR,plugin_file_name)
-		if os.path.exists(new_plugin_path_and_file):
-			d = gtk.MessageDialog(message_format="You already have a plugin with "+\
+		extension_path_and_file = extension_that_imported_me.f_globals['__file__']
+		extension_file_name = os.path.split(extension_path_and_file)[1]
+		new_extension_path_and_file = os.path.join(PREFERRED_EXTENSION_DIR, extension_file_name)
+		if os.path.exists(new_extension_path_and_file):
+			d = gtk.MessageDialog(message_format="You already have a extension with "+\
 					"the name %s installed; would you like to " +\
-					"replace it?" % os.path.splitext(plugin_file_name)[0],
+					"replace it?" % os.path.splitext(extension_file_name)[0],
 					type=gtk.MESSAGE_QUESTION)
 			d.add_buttons(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,'Replace',RESP_REPLACE)
 			d.set_default_response(RESP_REPLACE)
@@ -61,18 +61,18 @@ if thing_that_imported_plugin is None and \
 				sys.exit(0)
 		# confirm that the new path exists!
 		try:
-			os.makedirs(os.path.split(new_plugin_path_and_file)[0])
+			os.makedirs(os.path.split(new_extension_path_and_file)[0])
 		except:
 			pass # already exists
-		# and move the plugin
-		os.rename(plugin_path_and_file, new_plugin_path_and_file)
-		d = gtk.MessageDialog(message_format="Your new plugin is now available in "+\
+		# and move the extension
+		os.rename(extension_path_and_file, new_extension_path_and_file)
+		d = gtk.MessageDialog(message_format="Your new extension is now available in "+\
 		                      "Jokosher!",buttons=gtk.BUTTONS_OK)
 		d.destroy()
 		sys.exit(0)
 
 ############################################################################
-############# The actual plugin API ########################################
+############# The actual extension API ########################################
 ############################################################################
 
 class ExtensionAPI:
@@ -80,15 +80,58 @@ class ExtensionAPI:
 		pass
 		
 	def add_menu_item(self, menu_item_name, callback_function):
-		"Adds a menu item to a Jokosher menu."
+		"""
+		   Adds a menu item to a Jokosher extension menu.
+		"""
 		extensions_menu = self.mainapp.wTree.get_widget("extensionsmenu").get_submenu()
 		new_menu_item = gtk.MenuItem(menu_item_name)
 		new_menu_item.connect("activate", callback_function)
 		extensions_menu.prepend(new_menu_item)
+	
+	def play(self, play_state=True):
+		"""
+		   If play_state is True, it will play the project from the beginning.
+		   Otherwise, it will stop all playing.
+		"""
+		#Stop current playing (if any) and set to playhead to the beginning
+		self.mainapp.Stop()
+		if play_state:
+			#Commence playing
+			self.mainapp.Play()
+		
+	def stop(self):
+		"""
+		   Stops the project if it is currently playing.
+		   Same as play(play_state=False)
+		"""
+		self.mainapp.Stop()
+		
+	def add_file_to_selected_instrument(self, uri):
+		"""
+		   Creates a new event from the file at the given URI and 
+		   adds it to the selected instrument.
+		   Return values:
+		   0: success
+		   1: bad URI or file could not be loaded
+		   2: no instrument selected
+		"""
+		instr = None
+		for i in self.mainapp.project.instruments:
+			if i.isSelected:
+				instr = i
+				break
+		
+		if not instr:
+			#No instrument selected
+			return 2
+		
+		instr.addEventFromFile(0, uri)
+		#TODO: find out if the add failed and return 1
+		return 0
 
 
 def LoadAllExtensions():
-	for dir in PLUGIN_DIRS:
+	for dir in EXTENSION_DIRS:
 		if not os.path.isdir(dir):
 			continue
 		for f in os.listdir(dir):
@@ -102,7 +145,6 @@ def LoadAllExtensions():
 					print "done."
 				except:
 					print "failed."
-				
 				if file:
 					file.close()
 				
