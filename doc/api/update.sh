e433f73494ad4d125244730a74376157be8acd90
johng <>
2006-09-12 00:10:44 +0000

    Changed the update.sh script for the API docs a bit as the original borked on anything importing CommandManaged. Doing it all in one call to pydoc works if JokosherApp is done first.

--- doc/api/update.sh
+++ doc/api/update.sh
@@ -1,5 +1,12 @@
 export PYTHONPATH=../../Jokosher
-for doc in `ls ../../Jokosher/*.py | cut -d / -f 4 | cut -d . -f 1`
-do
-	pydoc -w $doc
+list="JokosherApp "
+for module in ../../Jokosher/*.py; do
+	name=`basename $module`
+	name=${name%.py}
+	if [ $name == "JokosherApp" ]; then continue; fi
+	# skip Profiler as this borks pydoc 
+	if [ $name == "Profiler" ]; then continue; fi 
+	list=$list$name" "
 done
+pydoc -w $list
+
