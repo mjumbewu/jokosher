45a5b108fe745afc20d25d81d451b7dc8104ecc0
davidc <>
2006-12-18 04:41:47 +0000

    Added help files, images and omf's to the installation. Version tag bumped to 0.9 Added the yelp and scrollkeeper registration process.

--- setup.py
+++ setup.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from subprocess import *
 import os
 import glob
 
+#Create an array with all the locale filenames
 I18NFILES = []
 for filepath in glob.glob("locale/*/LC_MESSAGES/*.mo"):
 	targetpath = os.path.dirname(os.path.join("share/", filepath))
 	I18NFILES.append((targetpath, [filepath]))
 
+#Create an array with all the help documents (docbook)
+HELPDOCS = []
+for filepath in glob.glob("help/jokosher/*/*.xml"):
+	targetpath = os.path.dirname(os.path.join("share/gnome/", filepath))
+	HELPDOCS.append((targetpath, [filepath]))
 
-setup(name='jokosher',
-	version='0.3',
+
+#Create an array with all the help images
+HELPIMAGES = []
+for filepath in glob.glob("help/jokosher/*/figures/*.png"):
+	targetpath = os.path.dirname(os.path.join("share/gnome/", filepath))
+	HELPIMAGES.append((targetpath, [filepath]))
+	
+#Check if scrollkeeper is available
+OMFFILES = []
+omfdir = None
+try:
+	process = Popen(args=["scrollkeeper-config", "--omfdir"], stdout=PIPE)
+except OSError:
+	pass
+	#Not available. Skip the registration.
+else:
+	#Obtain the OMF repository directory to install and register the help files
+	#This step only makes sense if run as root
+	if os.geteuid() == 0:
+		omfdir = os.path.join(process.stdout.read().strip(), "jokosher")
+		OMFFILES.append((omfdir, glob.glob("help/jokosher/*.omf")))
+
+dist = setup(name='jokosher',
+	version='0.9',
 	author='Jokosher Project',
 	author_email='email@jokosher.org',
 	maintainer='David Corrales',
@@ -21,10 +50,11 @@ setup(name='jokosher',
 	url='http://www.jokosher.org/',
 	download_url='http://www.jokosher.org/download',
 	license='GNU GPL',
-	platforms='linux-x86',
+	platforms='linux',
 	scripts=['jokosher'],
 	packages=['Jokosher'],
-	data_files=[('share/jokosher/',
+	data_files=[
+		('share/jokosher/',
 		glob.glob("Jokosher/*.glade")
 		),
 		('share/jokosher/',
@@ -51,12 +81,51 @@ setup(name='jokosher',
 		('share/jokosher/extensions',
 		glob.glob("extensions/*")
 		)
-		]+I18NFILES
+		]+I18NFILES+HELPDOCS+HELPIMAGES+OMFFILES
 )
 
+#Update the real URL attribute inside the OMF files and then
+#register the docs with scrollkeeper
+if omfdir != None and os.geteuid() == 0 and dist != None:
+	temp = "/tmp/jokosherTemp"
+	
+	#Non-documented way of getting the final directory prefix
+	installdir = dist.get_command_obj(command="install_data").install_dir
+	
+	#Create an array with the docbook file locations
+	HELPURI = []
+	for filepath in glob.glob("help/jokosher/*/jokosher.xml"):
+		targeturi = os.path.join(installdir, "share/gnome/", filepath)
+		HELPURI.append(targeturi)
+	
+	#Replace the URL placeholder inside the OMF files using sed
+	i = 0
+	for filepath in glob.glob(omfdir+"/*.omf"):
+		arguments = "sed 's|PATH_PLACEHOLDER|URI|' <INPUT >"+temp
+		arguments = arguments.replace("INPUT", filepath)
+		#We assume that the locale order between omf/docbook will stay the same
+		arguments = arguments.replace("URI", HELPURI[i])
+		
+		#Needs to go through a shell to redirect input/output
+		Popen(args=arguments, shell=True)
+
+		#Overwrite the old file with the updated one
+		call(["mv", temp, filepath])
+		
+		i += 1
+		
+	#Update the scrollkeeper catalog
+	print "\nUpdating the scrollkeeper index..."
+	call(["scrollkeeper-update", "-o", omfdir])
+	print "Installation finished! You can now run Jokosher by typing 'jokosher' or through your applications menu icon."
+	
 ## To uninstall manually delete these files/folders:
+## /usr/bin/jokosher
 ## /usr/share/jokosher/
+## /usr/share/gnome/help/jokosher/
 ## /usr/icons/hicolor/48x48/apps/jokosher-icon.png
 ## /usr/share/locale/*/LC_MESSAGES/jokosher.mo
 ## /usr/share/pixmaps/jokosher-icon.png
 ## /usr/share/applications/jokosher.desktop
+## /usr/lib/python2.X/site-packages/Jokosher/
+## omfdir/jokosher/*.omf
