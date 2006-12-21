164ee8654849d69b715bda72ab3dfea1fe9fd811
davidc <>
2006-12-21 07:07:03 +0000

    Updated docstrings (thanks jkelly).

--- Jokosher/AddInstrumentDialog.py
+++ Jokosher/AddInstrumentDialog.py
@@ -20,12 +20,20 @@ _ = gettext.gettext
 #=========================================================================
 
 class AddInstrumentDialog:
-	""" This class handles all of the processing associated with the
-		Add Instrument dialog.
-	"""	
+	"""
+	This class handles all of the processing associated with the Add Instrument dialog.
+	"""
 	#_____________________________________________________________________
 
 	def __init__(self, project, parent, instr=None):
+		"""
+		Creates a new instance of AddInstrumentDialog.
+		
+		Parameters:
+			project -- the Project to add instruments to.
+			parent -- parent window of the AddInstrumentDialog (JokosherApp).
+			instr -- if present, indicates the Instrument whose type wants to be changed.
+		"""
 		self.parent = parent
 		self.project = project
 		self.instr = instr
@@ -59,10 +67,10 @@ class AddInstrumentDialog:
 
 		self.model = gtk.ListStore(str, str, gtk.gdk.Pixbuf)
 			
-		for i in Globals.getCachedInstruments():
-			lineList = [x.center(12) for x in textwrap.wrap(i[0], 11)]
-			j = "\n".join(lineList)
-			self.model.append((j, i[1], i[2]))
+		for instr in Globals.getCachedInstruments():
+			lineList = [x.center(12) for x in textwrap.wrap(instr[0], 11)]
+			newList = "\n".join(lineList)
+			self.model.append((newList, instr[1], instr[2]))
 		
 		self.tree.set_model(self.model)
 			
@@ -86,18 +94,28 @@ class AddInstrumentDialog:
 	#_____________________________________________________________________
 	
 	def OnSelected(self, iconview, path):
-		"""An instrument is selected"""
+		"""
+		Calls the OnOK method when an instrument has been selected.
 		
+		Parameters:
+			iconview -- reserved for GTK callbacks, don't use it explicitly.
+			path -- reserved for GTK callbacks, don't use it explicitly.
+		"""
 		self.OnOK()
 
 	#_____________________________________________________________________
 			
 	def OnOK(self, button=None):
-		"""OK pushed on the dialog"""
+		"""
+		This method is called when the ok button in the dialog has been clicked.
+		It will then add the selected instrument into the main jokosher window (JokosherApp).  
 		
-		sel = self.tree.get_selected_items()
+		Parameters:
+			button -- reserved for GTK callbacks, don't use it explicity.
+		"""
+		selectedItems = self.tree.get_selected_items()
 		if not self.instr:
-			for i in sel:
+			for i in selectedItems:
 				item = self.model[i[0]]
 				#find the actual instrument using index 1 (the instrument type)
 				#because the name has been wrapped in self.model and can't be used
@@ -114,14 +132,24 @@ class AddInstrumentDialog:
 	#_____________________________________________________________________
 	
 	def OnCancel(self, button):
-		"""Cancel button is pressed"""
+		"""
+		Called when the cancel button in the dialog has been clicked.
 		
+		Parameters:
+			button -- reserved for GTK callbacks, dont't use it explicity.
+		"""
 		self.dlg.destroy()
 		
 	#_____________________________________________________________________
 
 	def OnSelectionChanged(self, button):
-		"""If a new instrument icon is chosen, this method is called"""
+		"""
+		Called when a new instrument icon is chosen.
+		The ok button in the dialog will appear inactive if there are no instruments selected.
+		
+		Parameters:
+			button -- reserved for GTK callbacks, dont't use it explicity.
+		"""
 		sel = self.tree.get_selected_items()
 
 		if len(sel) <= 0:
@@ -132,14 +160,19 @@ class AddInstrumentDialog:
 	#_____________________________________________________________________
 	
 	def OnSearchChange(self, widget):
-		"""A new letter is added to the search box, so update the search"""
+		"""
+		This method will be called when a new letter has been added to the search box.
+		It will then update the search results accordingly.
 		
+		Parameters:
+			widget -- reserved for GTK callbacks, dont't use it explicity.
+		"""
 		search_text = self.search_entry.get_text()
 		self.model = gtk.ListStore(str, str, gtk.gdk.Pixbuf)
 		
-		for i in Globals.getCachedInstruments():
-			if search_text.lower() in i[0].lower():
-				self.model.append(i)
+		for instr in Globals.getCachedInstruments():
+			if search_text.lower() in instr[0].lower():
+				self.model.append(instr)
 		
 		self.tree.set_model(self.model)
 		
