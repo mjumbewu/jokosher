31c81e49ce7f7bb8f68fe98ce3676ea8913cbb9b
johng <>
2007-01-03 18:41:07 +0000

    Added a new extension SetTempo which allows you to set the tempo for the project by tapping the beat during playback.

---
+++ extensions/eggs/SetTempo/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys, os
+from shutil import copy
+version="0.1"
+setup(name="SetTempo",
+version=version,
+description="Sets tempo for current project",
+packages=["SetTempo"],
+package_dir={"SetTempo":"src"},
+package_data={"":["SetTempo.glade"]},
+entry_points="""
+[jokosher.extensions]
+extension = SetTempo:SetTempo
+"""
+)
+# copy egg file to the deployment directory in the svn structure
+copy("dist/SetTempo-%s-py%d.%d.egg" % (version,sys.version_info[0],sys.version_info[1]),
+	 "../../") 
---
+++ extensions/eggs/SetTempo/src/SetTempo.py
@@ -0,0 +1,183 @@
+#	SetTempo.py
+#	-----------
+#	This extension allows you to set the tempo for imported audio
+#	by playing it back and tapping the space bar in time with the beat.
+import Jokosher.Extension
+import gtk
+import gtk.glade
+import pkg_resources
+import time
+	
+#=========================================================================
+
+class SetTempo:
+	"""
+	The class implements the SetTempo extension. It consists of
+	a dialog popped up by selecting the "Set Tempo" option
+	which is installed into the extensions menu by 
+	the startup method.
+	
+	The SetTempo extension allows you to determine the tempo for the
+	current project by tapping out the beat on the spacebar during
+	playback. It has it's own play and stop buttons to avoid 
+	switching back and forth with the main Jokosher window.
+	"""
+	EXTENSION_NAME = "Set Tempo"
+	EXTENSION_DESCRIPTION = "Sets the tempo for the current project by tapping in time with the music"
+	EXTENSION_VERSION = "0.9"
+	
+	#_____________________________________________________________________
+	
+	def startup(self, api):
+		"""
+		Called by the extension manager during Jokosher startup
+		or when the extension is added via ExtensionManagerDialog.
+		
+		Parameters:
+			api -- a reference to the Extension API
+		"""
+		self.API = api
+		self.menu_item = self.API.add_menu_item("Set Tempo", self.OnMenuItemClick)
+
+	#_____________________________________________________________________
+	
+	def shutdown(self):
+		"""
+		Called by the extension manager when the extension is
+		disabled or deleted.
+		"""
+		self.menu_item.destroy()
+
+	#_____________________________________________________________________
+	
+	def OnMenuItemClick(self, arg):
+		"""
+		GTK callback when the menu item added in startup()
+		is selected.
+		
+		Parameters:
+			arg -- set by GTK
+		"""
+		xmlString = pkg_resources.resource_string(__name__,"SetTempo.glade")
+		wTree = gtk.glade.xml_new_from_buffer(xmlString, len(xmlString))
+		
+		self.window = wTree.get_widget("SetTempoDialog")
+		self.tempoLabel = wTree.get_widget("tempoLabel")
+		signals = {
+			"OnClearClicked" : self.OnClear,
+			"OnStopClicked" : self.OnStop,
+			"OnPlayClicked" : self.OnPlay,
+			"OnTapClicked" : self.OnTap,
+			"OnSetClicked" : self.OnSet,
+			"OnCancelClicked" : self.OnCancel
+		}
+		wTree.signal_autoconnect(signals)
+		
+		self.window.set_transient_for(self.API.mainapp.window)
+		self.WriteLabel(0)
+		self.tappingTime = False
+		self.startTime = 0
+		self.prevTime = 0
+		self.intervals = 0
+		self.bpm = 0
+
+	#_____________________________________________________________________
+	
+	def OnClear(self, widget):
+		"""
+		GTK callback when the "Clear" button is clicked
+		
+		Parameters:
+			widget -- set by GTK
+		"""
+		self.tappingTime = False
+		self.WriteLabel(0)
+		self.bpm = 0
+		
+	#_____________________________________________________________________
+	
+	def OnStop(self, widget):
+		"""
+		GTK callback when the "Stop" button is clicked
+			
+		Parameters:
+			widget -- set by GTK
+		"""
+		self.API.stop()
+		
+	#_____________________________________________________________________
+	
+	def OnPlay(self, widget):
+		"""
+		GTK callback when the "Play" button is clicked
+			
+		Parameters:
+			widget -- set by GTK
+		"""
+		self.API.play()
+		
+	#_____________________________________________________________________
+	
+	def OnTap(self, widget):
+		"""
+		GTK callback when the "Tap" button is clicked
+		Note that none of the other buttons grab the focus
+		when clicked so the spacebar can easily be used to 
+		press this button. (Tapping the spacebar bar being
+		easier than clicking the mouse).
+			
+		Parameters:
+			widget -- set by GTK
+		"""
+		timeNow = time.time()
+		if self.tappingTime:
+			if timeNow - self.prevTime > 5:
+				print 'reset'
+				self.tappingTime = False
+				self.WriteLabel(0)
+			else:
+				self.intervals += 1
+				self.bpm = int(60 * self.intervals / (timeNow - self.startTime))
+				self.WriteLabel(self.bpm)
+				self.prevTime = timeNow
+				return
+		self.tappingTime = True
+		self.startTime, self.prevTime = timeNow, timeNow
+		self.intervals = 0
+		
+	#_____________________________________________________________________
+	
+	def OnSet(self, widget):
+		"""
+		GTK callback when the "Set" button is clicked
+			
+		Parameters:
+			widget -- set by GTK
+		"""
+		if self.bpm > 0:
+			self.API.set_bpm(self.bpm)
+			self.window.destroy()
+		
+	#_____________________________________________________________________
+	
+	def OnCancel(self, widget):
+		"""
+		GTK callback when the "Cancel" button is clicked
+			
+		Parameters:
+			widget -- set by GTK
+		"""
+		self.window.destroy()
+		
+	#_____________________________________________________________________
+	
+	def WriteLabel(self, value):
+		"""
+		Formats and writes the bpm value
+		"""
+		self.tempoLabel.set_markup("<span font_desc='Sans Bold 32'>%d bpm</span>" % value)
+		
+	#_____________________________________________________________________
+	
+#=========================================================================
+		
