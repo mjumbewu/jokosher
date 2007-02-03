94f72a1dffd173c2f73ac59ad7bc63f5609a81e7
luket <>
2007-02-03 20:53:32 +0000

    the instrument type manager stuff

---
+++ extensions/eggs/InstrumentTypeManager/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys, os
+from shutil import copy
+version="0.2"
+setup(name="InstrumentTypeManager",
+version=version,
+description="Adds or Removes an instrument type from jokosher",
+packages=["InstrumentTypeManager"],
+package_dir={"InstrumentTypeManager":"src"},
+package_data={"":["InstrumentTypeManager.glade"]},
+entry_points="""
+[jokosher.extensions]
+extension = InstrumentTypeManager:InstrumentTypeManager
+"""
+)
+# copy egg file to the deployment directory in the svn structure
+copy("dist/InstrumentTypeManager-%s-py%d.%d.egg" % (version,sys.version_info[0],sys.version_info[1]),
+	 "../../") 
