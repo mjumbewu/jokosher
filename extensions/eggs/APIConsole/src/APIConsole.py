9db0f8ec5825fdc0dd1df0521f9575de94e7bd14
davidc <>
2007-02-03 02:28:48 +0000

    Minimal cosmetic fixes. Also added an "exit" command to close the terminal.

--- extensions/eggs/APIConsole/src/APIConsole.py
+++ extensions/eggs/APIConsole/src/APIConsole.py
@@ -1,3 +1,4 @@
+#
 #	API Console
 #	-----------
 #	This extension is a simple Console for controlling jokosher via
@@ -6,6 +7,8 @@
 #	simple to use: just type in an API function name
 #	(e.g. add_instrument("cello", "My Cello")) and hit enter. The
 #	return value (if any) is prtined back at you. rinse and repeat
+#
+#-------------------------------------------------------------------------------
 
 import Jokosher.Extension
 import gtk
@@ -14,62 +17,110 @@ import os
 import pkg_resources
 import Globals
 
+#=========================================================================
+
 class APIConsole:
+	"""
+	Displays a terminal to execute Jokosher Extension API commands.
+	"""
+	
 	EXTENSION_NAME = "API Console"
 	EXTENSION_DESCRIPTION = "Offers a simple console to acces the Jokosher API"
 	EXTENSION_VERSION = "0.0.1"
 	
-	def OnMenuItemClick(self,arg):
+	#_____________________________________________________________________
+	
+	def startup(self, api):
+		"""
+		Initializes the extension.
+		
+		Parameters:
+			api -- reference to the Jokosher extension API.
+		"""
+		self.api = api
+		self.menu_item = self.api.add_menu_item("API Console", self.OnMenuItemClick)
+		self.defaultOutput = "dir - a list of api functions\n" + \
+					"help <function> - show documentation for a function\n" + \
+					"clear - clear text buffer\n" + \
+					"exit - close the API Console\n" + \
+					"------------------------------------\n"
+
+	#_____________________________________________________________________
+
+	def shutdown(self):
+		"""
+		Destroys any object created by the extension when it is disabled.
+		"""
+		self.menu_item.destroy()
+
+	#_____________________________________________________________________
+	
+	def OnMenuItemClick(self, menuItem):
+		"""
+		Called when the user clicks on this extension's menu item.
+		
+		Parameters:
+			menuItem -- reserved for GTK callbacks. Don't use it explicitly.
+		"""
 		xmlString = pkg_resources.resource_string(__name__,"APIConsole.glade")
 		wTree = gtk.glade.xml_new_from_buffer(xmlString, len(xmlString),"APITestDialog")
-		signal = {"on_Activate" : self.Execute}
-		wTree.signal_autoconnect(signal)
-		window = wTree.get_widget("APITestDialog")
-		self.API.set_window_icon(window)
-		self.command = wTree.get_widget("entry1")
-		self.output = wTree.get_widget("textview1")
-		self.scrollwindow = wTree.get_widget("scrolledwindow1")
+		
+		signals = 	{
+					"on_Activate" : self.Execute
+					}
+		wTree.signal_autoconnect(signals)
+		
+		self.window = wTree.get_widget("APITestDialog")
+		self.command = wTree.get_widget("entryCommand")
+		self.output = wTree.get_widget("textviewOutput")
+		self.scrollwindow = wTree.get_widget("scrolledwindow")
+		
+		self.api.set_window_icon(self.window)
 		self.output_text = gtk.TextBuffer()
-		self.output_text.insert_at_cursor("dir - a list of API functions\n" +
-					"help <function> - show documentation for a function\n" +
-					"clear - clear text buffer\n"
-					"--------------------------------\n")
+		self.output_text.insert_at_cursor(self.defaultOutput)
 		self.output.set_buffer(self.output_text)
 		self.output.scroll_mark_onscreen(self.output_text.get_insert())
 		
 		self.completion_model = gtk.ListStore(str)
 		
-		window.show_all()
+		self.window.show_all()
 	
+	#_____________________________________________________________________
 	
-	def Execute(self, arg):
-		self.output_text.insert_at_cursor(">>>>"+self.command.get_text()+"\n")
+	def Execute(self, entry):
+		"""
+		Executes the command given by the user.
+		
+		Parameters:
+			entry -- reserved for GTK callbacks. Don't use it explicitly.
+		"""
+		self.output_text.insert_at_cursor(">>>>%s\n" % self.command.get_text())
 		
 		if self.command.get_text() == "dir" or self.command.get_text() == "ls":
 			outputList = []
-			for i in dir(self.API):
-				i = getattr(self.API, i)
-				if callable(i):
-					outputList.append(i.__name__)
-			self.output_text.insert_at_cursor("\n".join(outputList)+"\n\n")
+			for method in dir(self.api):
+				method = getattr(self.api, method)
+				if callable(method):
+					outputList.append(method.__name__)
+			self.output_text.insert_at_cursor("\n".join(outputList) + "\n\n")
 					
 		elif self.command.get_text().startswith("help"):
 			cmd = self.command.get_text()[4:].strip()
-			if hasattr(self.API, cmd):
-				i = getattr(self.API, cmd)
-				self.output_text.insert_at_cursor(i.__name__+":\n"+i.__doc__+"\n\n")
+			if hasattr(self.api, cmd):
+				method = getattr(self.api, cmd)
+				self.output_text.insert_at_cursor("%s:\n%s\n\n" % (method.__name__, method.__doc__))
 		
 		elif self.command.get_text() == "clear":
-			self.output_text.set_text("dir - a list of API functions\n" +
-					"help <function> - show documentation for a function\n" +
-					"clear - clear text buffer\n"
-					"--------------------------------\n")
+			self.output_text.set_text(self.defaultOutput)
 		
+		elif self.command.get_text() == "exit":
+			self.window.destroy()
+			
 		else:
 			try:
-				rvalue = eval("self.API."+self.command.get_text())
+				rvalue = eval("self.api.%s" % self.command.get_text())
 				if rvalue:
-					self.output_text.insert_at_cursor(str(rvalue)+"\n\n")
+					self.output_text.insert_at_cursor(str(rvalue) + "\n\n")
 			except:
 				self.output_text.insert_at_cursor("Malformed function call, unimplimented function, or some random exception!\n\n")
 		
@@ -77,12 +128,8 @@ class APIConsole:
 		self.output.set_buffer(self.output_text)
 		self.output.scroll_mark_onscreen(self.output_text.get_insert())
 		
+	#_____________________________________________________________________
+	
 	#You know you're a Newfie when: You think the first day of salmon fishing season is a provincial holiday
-	
-	
-	def startup(self, api):
-		self.API = api
-		self.menu_item = self.API.add_menu_item("API Console", self.OnMenuItemClick)
 
-	def shutdown(self):
-		self.menu_item.destroy()
+#=========================================================================
