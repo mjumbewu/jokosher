0df0b897ee4c483059c37c36235e43660344dfd8
Mike Sheldon <mike@mikeasoft.com>
2008-08-09 01:23:01 +0100

    * Populate list of projects which can be restored

--- Jokosher/CrashProtectionDialog.py
+++ Jokosher/CrashProtectionDialog.py
@@ -8,6 +8,10 @@
 
 import gtk.glade
 import gobject
+import os, time
+import xml.dom.minidom as xml
+import gzip
+import Utils
 import Globals
 import gettext
 _ = gettext.gettext
@@ -37,4 +41,30 @@ class CrashProtectionDialog:
 
 
 	def populate(self):
-		pass
+		backupDir = os.path.join(os.path.expanduser("~"), ".jokosher", "backups")
+		crashTable = self.crashedProjectTree.get_widget("crashTable")
+		row = 1
+		for backupFile in os.listdir(backupDir):
+			backup = os.path.join(backupDir, backupFile)
+			Globals.debug("Found backup file: %s" % backup)
+			try:
+				if os.stat(backup).st_size == 0:
+					#This backup file was never written to, so just delete it
+					os.remove(backup)
+					continue
+				backupFD = gzip.GzipFile(backup, "r")
+				backupXML = xml.parse(backupFD)
+				backupDict = Utils.LoadDictionaryFromXML(backupXML.getElementsByTagName('Parameters')[0])
+				saveTime = int(backupFile.split("-")[0])
+				name = backupDict["name"]
+				hbox = gtk.HBox(3)
+				crashTable.attach(gtk.Label(name), 0, 1, row, row+1)
+				crashTable.attach(gtk.Label(time.ctime(saveTime)), 1, 2, row, row+1)
+				crashTable.attach(gtk.Button("Restore"), 2, 3, row, row+1)
+				crashTable.attach(gtk.Button("Delete"), 3, 4, row, row+1)
+				row+=1
+				except:
+					Globals.debug("Couldn't read backup file: %s" % backup)
+		crashTable.show_all()
+
+
