a3a95e81ec44d338c1583d53da0e368440dd6d02
Laszlo Pandy <bzr@laszlopandy.com>
2009-02-18 00:33:50 -0500

    Add script to automate merging of translations from launchpad.

---
+++ locale/src/rosetta-merge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, sys
+
+def main():
+	if len(sys.argv) != 2:
+		print "Usage: %s <launchpad-download-folder>" % sys.argv[0]
+		return
+	
+	files_to_merge = []
+	reldir = sys.argv[1]
+	for name in os.listdir(reldir):
+		if name.endswith(".po") and os.path.exists(name):
+			files = (os.path.join(reldir, name), name)
+			files_to_merge.append(files)
+			
+	print "Ready to merge %d PO files." % len(files_to_merge)
+	for dl_file, file in files_to_merge:
+		merge_files(dl_file, file)
+	print "Done."
+	
+	
+def merge_files(rosetta_download, bzr_version):
+	COMMAND = 'msgmerge "%s" "%s" -o "%s"'
+	
+	outfile = bzr_version + ".tmp"
+	cmd = COMMAND % (rosetta_download, bzr_version, outfile)
+	
+	print "=> msgmerge-ing", bzr_version
+	os.system(cmd)
+	os.rename(outfile, bzr_version)
+	
+if __name__ == "__main__":
+	main()
