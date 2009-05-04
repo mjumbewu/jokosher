c1888d67812dbfc3e383473544f789f7851fbaf0
Mike Sheldon <mike@mikeasoft.com>
2009-05-04 10:33:13 +0100

    Send user to http://doc.jokosher.org if gtk.show_uri is unavailable

--- Jokosher/Utils.py
+++ Jokosher/Utils.py
@@ -39,7 +39,11 @@ def OpenExternalURL(url, message, parent, timestamp=0):
 	"""
 		
 	screen = gtk.gdk.screen_get_default()
-	ret = gtk.show_uri(screen, url, timestamp)
+	ret = False
+	try:
+		ret = gtk.show_uri(screen, url, timestamp)
+	except:
+		ret = webbrowser.open(url)
 	
 	if not ret and message:
 		dlg = gtk.MessageDialog(parent,
